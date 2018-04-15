2190a092b9f9374598861dace1e176356039cb48
michaelKaefer <michael.kaefer1@gmx.at>
2018-04-15 21:42:30 +0200

    initial commit

---
+++ src/michaelkaefer/oauth2-amazon/src/Provider/Amazon.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace MichaelKaefer\OAuth2AmazonForPrestashop17\MichaelKaefer\OAuth2\Client\Provider;
+
+use MichaelKaefer\OAuth2ClientForPrestashop17\League\OAuth2\Client\Provider\AbstractProvider;
+use MichaelKaefer\GuzzleHttp6ForPrestashop17\Psr\Http\Message\ResponseInterface;
+use MichaelKaefer\OAuth2ClientForPrestashop17\League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use MichaelKaefer\OAuth2ClientForPrestashop17\League\OAuth2\Client\Token\AccessToken;
+use MichaelKaefer\OAuth2ClientForPrestashop17\League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+
+class Amazon extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+
+    /**
+     * Get authorization url to begin OAuth flow
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return 'https://www.amazon.com/ap/oa';
+    }
+
+    /**
+     * Get access token url to retrieve token
+     *
+     * @param array $params
+     *
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return 'https://api.amazon.com/auth/o2/token';
+    }
+
+    /**
+     * Get provider url to fetch user details
+     *
+     * @param AccessToken $token
+     *
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return 'https://api.amazon.com/user/profile';
+    }
+
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return ['profile'];
+    }
+
+    /**
+     * Returns the string that should be used to separate scopes when building
+     * the URL for requesting an access token.
+     *
+     * @return string Scope separator, defaults to ','
+     */
+    protected function getScopeSeparator()
+    {
+        return ' ';
+    }
+
+    /**
+     * Check a provider response for errors.
+     *
+     * @param  ResponseInterface $response
+     * @param  array|string $data
+     *
+     * @throws IdentityProviderException
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if (isset($data['error'])) {
+            $statusCode = $response->getStatusCode();
+            $error = $data['error'];
+            $errorDescription = $data['error_description'];
+            $errorLink = (isset($data['error_uri']) ? $data['error_uri'] : false);
+            throw new IdentityProviderException(
+                $statusCode . ' - ' . $errorDescription . ': ' . $error . ($errorLink ? ' (see: ' . $errorLink . ')' : ''),
+                $response->getStatusCode(),
+                $response
+            );
+        }
+    }
+
+    /**
+     * Generate a user object from a successful user details request.
+     *
+     * @param array $response
+     * @param AccessToken $token
+     *
+     * @return MichaelKaefer\OAuth2ClientForPrestashop17\League\OAuth2\Client\Provider\ResourceOwnerInterface
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new AmazonResourceOwner($response);
+    }
+
+    /**
+     * Returns a prepared request for requesting an access token.
+     *
+     * @param array $params
+     *
+     * @return MichaelKaefer\GuzzleHttp6ForPrestashop17\Psr\Http\Message\RequestInterface
+     */
+    protected function getAccessTokenRequest(array $params)
+    {
+        $request = parent::getAccessTokenRequest($params);
+        $uri = $request->getUri()
+            ->withUserInfo($this->clientId, $this->clientSecret);
+        return $request->withUri($uri);
+    }
+}
